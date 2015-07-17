01a71016c99c46a82d8464dd31463fedb318ddd2
kaazoo <kaazoo@drqueue.org>
2015-07-17 15:57:05 +0200

    fix indention filter paths

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,11 @@
 class SearchController < ApplicationController
 
-	def index
-	  @frequency = FORM_FREQ.to_i
-	end
+  def index
+    @frequency = FORM_FREQ.to_i
+  end
 
-
-	def ajax_search
-		if params['search'] && params['search'].length >= QUERY_MIN_LENGTH
-
+  def ajax_search
+    if params['search'] && params['search'].length >= QUERY_MIN_LENGTH
       @query = URI.unescape(params['search'])
       @query = ActionController::Base.helpers.sanitize(@query)
       @query_not_escaped = @query
@@ -19,19 +17,31 @@ class SearchController < ApplicationController
         locate_cmd = "locate"
       end
 
-			cmd = locate_cmd + " " + LOCATE_ARGS.to_s + " -l "+ RESULT_LIMIT.to_s + " " + @query
-			puts cmd
+	  cmd = locate_cmd + " " + LOCATE_ARGS.to_s + " -l "+ RESULT_LIMIT.to_s + " " + @query
+	  Rails.logger.debug(cmd)
 
-			out = IO.popen(cmd)
-			@results = out.readlines
-			@results_num = @results.length
-		
-			render :layout => false
-		else
-			render :text => "Query must be longer than " + QUERY_MIN_LENGTH.to_s + " characters."
-		end
+      out = IO.popen(cmd)
+	  all_results = out.readlines
+      @results = []
+
+      Rails.logger.debug("All results: " + all_results.length.to_s)
+      all_results.each do |line|
+        if line.match(ALLOWED_PREFIXES)
+          @results << line
+        end
+      end
+      Rails.logger.debug("Allowed results: " + @results.length.to_s)
+
+      @results.each do |line|
+        Rails.logger.debug(line)
+      end
+
+	  @results_num = @results.length
+
+	  render :layout => false
+	else
+	  render :text => "Query must be longer than " + QUERY_MIN_LENGTH.to_s + " characters."
 	end
-
-
+  end
 
 end
