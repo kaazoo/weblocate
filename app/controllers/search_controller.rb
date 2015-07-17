899e341d9b5dd7b550d28b2a9bc58c80ddda74e5
kaazoo <kaazoo@drqueue.org>
2015-07-17 16:47:47 +0200

    run multiple searches use array for prefixes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,20 +17,21 @@ class SearchController < ApplicationController
         locate_cmd = "locate"
       end
 
-	  cmd = locate_cmd + " " + LOCATE_ARGS.to_s + " -l "+ RESULT_LIMIT.to_s + " " + @query
-	  Rails.logger.debug(cmd)
-
-      out = IO.popen(cmd)
-	  all_results = out.readlines
       @results = []
 
-      Rails.logger.debug("All results: " + all_results.length.to_s)
-      all_results.each do |line|
-        if line.match(ALLOWED_PREFIXES)
-          @results << line
-        end
+      prefixes = ALLOWED_PREFIXES
+      prefixes.each do |prefix|
+        cmd = locate_cmd + " " + LOCATE_ARGS.to_s + " -l "+ RESULT_LIMIT.to_s + " " + prefix + "/\*" + @query
+        Rails.logger.debug(cmd)
+
+        out = IO.popen(cmd)
+        cmd_results = out.readlines
+        
+        Rails.logger.debug("CMD Results: " + cmd_results.length.to_s)
+        @results += cmd_results
       end
-      Rails.logger.debug("Allowed results: " + @results.length.to_s)
+
+      Rails.logger.debug("All results: " + @results.length.to_s)
 
       @results.each do |line|
         Rails.logger.debug(line)
